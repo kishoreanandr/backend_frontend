6cf312ccd5eeb164e109d6dbe2736310b59d2f3a
kishoreanand.r <kishoreanand.r@gmail.com>
2024-01-24 16:25:26 +0530

    React_basics added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Testcompoenent1 from './components/functional_component/test_component';
+// import Reitreatorclasscomp from './components/class_components/classcomponent';
+import classlearn from './components/class_components/classcomponent';
+import Login from './components/class_components/loginComponent';
 function App() {
   return (
     <div className="App">
+      {/* <Reitreatorclasscomp></Reitreatorclasscomp> */}
+      <classlearn></classlearn>
+      <Login></Login>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Testcompoenent1></Testcompoenent1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/functional_component/test_component.jsx
@@ -0,0 +1,23 @@
+//Component => Set of code which we call in many files
+
+import React from "react"; //if your render in the same file your import
+// import Reitreatorclasscomp from "../class_components/classcomponent";
+// function Testcompoenent()
+// {
+//     return (
+//         <div>
+//             <h1>This is a Test Functional Component</h1>
+//             <p>We are learning</p>
+//         </div>
+//     )
+// }
+function Testcompoenent1()
+{
+    return (<div>
+        {/* <Reitreatorclasscomp></Reitreatorclasscomp> */}
+        <p>
+        Functional Component in React</p></div>)
+}
+
+
+export default Testcompoenent1;  //If you want to use this component name
